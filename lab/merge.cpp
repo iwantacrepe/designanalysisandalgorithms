6fe7d49d42711a4d1a8b63098eba69dd7107b51d
iwantacrepe <100364726+iwantacrepe@users.noreply.github.com>
2024-05-09 06:57:06 +0530

    Update merge.cpp

--- lab/merge.cpp
+++ lab/merge.cpp
@@ -5,16 +5,16 @@ void merge(int arr[], int l, int m, int r) {
     int n1 = m - l + 1;
     int n2 = r - m;
 
-    // Create temp arrays
+    
     int L[n1], R[n2];
 
-    // Copy data to temp arrays
+    
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
 
-    // Merge the temp arrays back into arr[l..r]
+    
     i = 0;
     j = 0;
     k = l;
@@ -29,14 +29,14 @@ void merge(int arr[], int l, int m, int r) {
         k++;
     }
 
-    // Copy the remaining elements of L[], if any
+    
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    // Copy the remaining elements of R[], if any
+    
     while (j < n2) {
         arr[k] = R[j];
         j++;
